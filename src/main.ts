0d2f4b295039937ca570783ed506a4f3a75bcb73
Sang Nguyen <sanggait1996@gmail.com>
2024-12-28 01:06:31 +0700

    setup/swagger

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,28 @@ import { NestFactory } from '@nestjs/core';
 import helmet from 'helmet';
 import { AppModule } from './app.module';
 import { Logger } from 'nestjs-pino';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useLogger(app.get(Logger));
   app.use(helmet());
+
+  app.setGlobalPrefix(`${process.env.API_PREFIX}/${process.env.API_VERSION}`);
+
+  const config = new DocumentBuilder()
+    .setTitle('Revents API Documentation')
+    .setDescription('Revents API description for application')
+    .setVersion(process.env.API_VERSION)
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup(
+    `${process.env.API_PREFIX}/${process.env.API_VERSION}/docs`,
+    app,
+    document,
+  );
 
   const configService = app.get(ConfigService);
   const port = configService.get('port');
